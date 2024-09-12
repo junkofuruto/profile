805a597cd133fcf7f4f62d36d5fcfeec5f6f6196
Theodore Kaczynski <ah3082340@gmail.com>
2024-09-12 23:54:21 +0300

    addition: https support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
   plugins: [solid()],
   server:
   {
-    host: "192.168.0.10",
-    port: "30080"
+    host: "0.0.0.0",
+    port: "443",
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs', 'key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'cert.pem')),
+    },
   },
   assetsInclude: ['**/*.glb', '**/*.gltf']
 })
