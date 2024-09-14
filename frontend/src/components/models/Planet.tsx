a157ea1627977cd96cad18cc4938ab33bc726c02
Theodore Kaczynski <ah3082340@gmail.com>
2024-09-14 21:08:35 +0300

    fix: fixed asset loading error

--- frontend/src/components/models/Planet.tsx
+++ frontend/src/components/models/Planet.tsx
@@ -2,12 +2,14 @@ import { Component } from "solid-js";
 import { Scene, loading } from "./Scene";
 import { Euler, Vector3 } from "three";
 
+import model from "../../assets/planet.glb";
+
 export const isLoading = loading;
 
 export const Planet: Component = () => {
     return (
         <Scene
-            model="src/assets/h_planet.glb"
+            model={model}
             delta={0.005}
             scale={new Vector3(0.03, 0.03, 0.03)}
             position={new Vector3(2, 1, 0)}
