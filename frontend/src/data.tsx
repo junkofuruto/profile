212d63330d597f8cad7d7f92a4ec32cb146daec8
Theodore Kaczynski <ah3082340@gmail.com>
2024-09-15 01:20:50 +0300

    addition: docker compose support

--- frontend/src/data.tsx
+++ frontend/src/data.tsx
@@ -3,26 +3,26 @@ export const data = {
         hero: {
             album: "TO BE ADDICTED",
             author: "CKD4",
-            image_url: "/images/artists/releases/bb49aed0-09f9-4251-af4a-e4d464c98c66.jpg",
+            image_url: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/bb49aed0-09f9-4251-af4a-e4d464c98c66.jpg?alt=media&token=2ec25ca7-40d3-470f-a104-e4d0cebc9d02",
             link: "https://band.link/ckd4",
         },
         artists: [
             {
                 name: "konstrukt",
                 genre: "PUNK ROCK / POST PUNK",
-                image_url: "/images/artists/konstrukt.png",
+                image_url: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/konstrukt.png?alt=media&token=4c8aab88-9339-4f65-9501-0078c4d4bb94",
                 href: "https://band.link/construct"
             },
             {
                 name: "ckd4",
                 genre: "EDM / DARK AMBIENT",
-                image_url: "/images/artists/ckd4.png",
+                image_url: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/ckd4.png?alt=media&token=dc144e4f-0c93-4c51-b068-a986dea490e9",
                 href: "https://band.link/ckd4"
             },
             {
                 name: "DON'T SLEEP!",
                 genre: "GARAGE ROCK / BLACK NOISE",
-                image_url: "/images/artists/dontsleep.png",
+                image_url: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/dontsleep.png?alt=media&token=00a1835e-beef-4210-9dba-17796a1b7590",
                 href: "https://band.link/J2QUb"
             }
         ]
@@ -33,35 +33,35 @@ export const data = {
                 tags: "APPLICATION / MOBILE",
                 date: "2023 Q3",
                 name: "safit",
-                preview: "/images/projects/safit.jpg",
+                preview: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/safit.jpg?alt=media&token=afd081a4-82bb-44b9-bfcc-1e8817603061",
                 info: ""
             },
             {
                 tags: "GAME / BOT",
                 date: "2024 Q1",
                 name: "dadagames",
-                preview: "/images/projects/dadagames.jpg",
+                preview: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/dadagames.jpg?alt=media&token=7fc78f35-3ca0-456b-a4a5-614eb0f26a12",
                 info: ""
             },
             {
                 tags: "UTILS / NET",
                 date: "2023 Q3",
                 name: "ddef",
-                preview: "/images/projects/ddef.jpg",
+                preview: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/ddef.jpg?alt=media&token=de7ad90b-1ec8-4e59-845a-808f1a6d4865",
                 info: ""
             },
             {
                 tags: "UTILS / OPENAI / BOT",
                 date: "2023 Q3",
                 name: "gptbot",
-                preview: "/images/projects/gptbot.jpg",
+                preview: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/gptbot.jpg?alt=media&token=b72880db-fcca-428d-b9f6-1705cb1bef02",
                 info: ""
             },
             {
                 tags: "UTILS / BOT",
                 date: "2023 Q3",
                 name: "qrcodebot",
-                preview: "/images/projects/qrcodebot.jpg",
+                preview: "https://firebasestorage.googleapis.com/v0/b/constructmg-eb3ee.appspot.com/o/qrcodebot.jpg?alt=media&token=4d213b0e-43bd-47ee-85cd-3b783d66e8c3",
                 info: ""
             },
         ]
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,11 +9,7 @@ export default defineConfig(() => {
         server:
         {
             host: "0.0.0.0",
-            port: "443",
-            https: {
-                key: fs.readFileSync(path.resolve(__dirname, 'certs', 'key.pem')),
-                cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'cert.pem')),
-            },
+            port: "80",
             hmr: {
                 overlay: false
             }
